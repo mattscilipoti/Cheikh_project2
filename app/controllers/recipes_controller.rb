82f68384327e803c9c76cc0302587c1c3d5c88dd
Cheikh Ndiongue <Cmndiongue@yahoo.com>
2015-08-03 13:45:09 -0400

    The update functionaliy is working. I had to look trough previous homework project as a guide on this one

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -20,9 +20,13 @@ class RecipesController < ApplicationController
   end
 
   def edit
+    @recipe = Recipe.find(params[:id])
   end
 
   def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to @recipe
   end
 
   def destroy
