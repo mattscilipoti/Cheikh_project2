ec147e437109371d29c1531142da1a84780865e5
Cheikh Ndiongue <Cmndiongue@yahoo.com>
2015-08-03 12:16:37 -0400

    created recipe model and added the new, create and show within the recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,22 @@
 class RecipesController < ApplicationController
+  before_action :find_recipe, only: [:edit, :show, :update, :destroy]
 
   def index
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+  end
+
+  def show
+  end
+
+  private
+  def find_recipe
+    @recipe = Recipe.find(params[:id])
   end
 end
